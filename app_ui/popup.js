6279a558b8a5c28ae80296df54f04edf9d7dec93
stasfr <stas240600@yandex.ru>
2022-07-16 02:55:02 +0300

    "redesign, added footer, todo: fix popup bug"

--- app_ui/popup.js
+++ app_ui/popup.js
@@ -2,20 +2,27 @@
 const testsPopup = document.querySelector('.tests_popup')
 
 document.querySelector('#tests_button').addEventListener('click', () => {
-    testsPopup.style.display = 'flex'
+    testsPopup.classList.toggle('tests_popup_active')
 })
 
 document.querySelector('#schwarz_test_choose').addEventListener('click', () => {
-    testsPopup.style.display = 'none'
+    testsPopup.classList.toggle('tests_popup_active')
 })
+
 
 // Contacts popup 
 const contactsPopup = document.querySelector('.contacts_popup')
 
 document.querySelector('#contacts_button').addEventListener('click', () => {
-    contactsPopup.style.display = 'flex'
+    contactsPopup.classList.toggle('contacts_popup_active')
 })
 
 document.querySelector('#contacts_popup_exit_button').addEventListener('click', () => {
-    contactsPopup.style.display = 'none'
+    contactsPopup.classList.toggle('contacts_popup_active')
+})
+
+
+//Tests
+document.querySelector('.coming_soon_button').addEventListener('click', () => {
+    console.log('done');
 })
